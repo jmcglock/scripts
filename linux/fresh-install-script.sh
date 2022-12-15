0b76221a560abc4e789e42d649148e71155f4b63
JMcglock <85720969+jmcglock@users.noreply.github.com>
2022-12-15 15:19:28 -0500

    small adjustments

--- linux/fresh-install-script.sh
+++ linux/fresh-install-script.sh
@@ -2,12 +2,15 @@
 
 # run this after a fresh install of ubuntu server
 # Check for and install updates
-apt-get update
-apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 # Install Docker
-apt-get install docker -y
+sudo apt-get install docker -y
 
 # Create a new sudo user named "YOUR NAME"
 adduser YOUR NAME
 usermod -aG sudo YOUR NAME
+
+# Reboot
+sudo reboot
