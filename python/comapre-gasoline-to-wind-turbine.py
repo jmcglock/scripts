88b164f0999d669c530a8b786b99a710dc5841ae
JMcglock <85720969+jmcglock@users.noreply.github.com>
2022-12-15 12:59:16 -0500

    what even is a btu

--- python/comapre-gasoline-to-wind-turbine.py
+++ python/comapre-gasoline-to-wind-turbine.py
@@ -7,12 +7,15 @@ gasoline_efficiency = 20
 # Set the average capacity factor of a wind turbine (in percent)
 wind_capacity_factor = 35
 
-# Calculate the average amount of energy produced per gallon of gasoline
+# Calculate the average amount of energy produced per gallon of gasoline (in Btu)
 gasoline_output = gasoline_energy * (gasoline_efficiency / 100)
 
-# Calculate the average amount of energy produced per kilowatt-hour of wind energy
-wind_output = 1000 * (wind_capacity_factor / 100)
+# Convert the gasoline output from Btu to kWh
+gasoline_output_kwh = gasoline_output * 0.00029307107
+
+# Calculate the average amount of energy produced per kWh of wind energy
+wind_output_kwh = 1 * (wind_capacity_factor / 100)
 
 # Print the result
-print(f"One gallon of gasoline produces {gasoline_output} British Thermal Units of energy, "
-      f"compared to {wind_output} kilowatt-hours of wind energy.")
+print(f"One gallon of gasoline produces {gasoline_output_kwh:.3f} kilowatt-hours of energy, "
+      f"compared to {wind_output_kwh:.3f} kilowatt-hours of wind energy.")
