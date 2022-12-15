7614179a63ca9c56a2ea28a9541b4cac3461eeb1
JMcglock <85720969+jmcglock@users.noreply.github.com>
2022-12-15 14:55:28 -0500

    nifty

---
+++ linux/setup-pihole-instance.sh
@@ -0,0 +1,13 @@
+# Update the package manager and upgrade installed packages
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install the required dependencies
+sudo apt-get install git dnsutils bc
+
+# Clone the Pi-hole installation script from GitHub
+git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+
+# Navigate to the Pi-hole directory and run the installation script
+cd Pi-hole
+sudo bash install.sh
