f8ec076a7d02ad7eb3ffeb6b6d80afac41b102f9
JMcglock <85720969+jmcglock@users.noreply.github.com>
2022-12-15 12:51:15 -0500

    when tesla

---
+++ python/when-tesla.py
@@ -0,0 +1,14 @@
+# Set the current price of a Tesla Model 3
+price = 35000
+
+# Set the monthly savings amount
+savings = 1000
+
+# Calculate the number of months it would take to save enough money
+months = price / savings
+
+# Calculate the number of years it would take to save enough money
+years = months / 12
+
+# Print the result
+print(f"It would take {years:.1f} years to save enough money to buy a Tesla.")
