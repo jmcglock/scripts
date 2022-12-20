087cd6c0d22045e81b18b0bb9dc38002872bba53
JMcglock <85720969+jmcglock@users.noreply.github.com>
2022-12-20 13:40:11 -0500

    removed old packages

--- linux/fresh-ubuntu-desktop-install.sh
+++ linux/fresh-ubuntu-desktop-install.sh
@@ -11,24 +11,12 @@ echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable ma
 sudo apt update
 sudo apt install brave-browser -y
 
-# Install Microsoft Edge Beta
-wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-dev/microsoft-edge-dev_91.0.864.55-1_amd64.deb
-sudo dpkg -i microsoft-edge-dev_91.0.864.55-1_amd64.deb
-sudo apt install -f
-
 # Install Visual Studio Code
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update
 sudo apt install code -y
 
-# Install JetBrains Toolbox
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.21.7135.tar.gz
-tar -xzf jetbrains-toolbox-1.21.7135.tar.gz
-./jetbrains-toolbox-1.21.7135/jetbrains-toolbox
-
-# Install MySQL Workbench
-sudo apt install mysql-workbench -y
 
 # Install Stacer
 wget https://github.com/oguzhaninan/Stacer/releases/download/v1.1.0/stacer_1.1.0_amd64.deb
@@ -40,25 +28,9 @@ wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=d
 sudo dpkg -i discord.deb
 sudo apt install -f
 
-# Install Termius
-wget https://downloads.termius.com/linux/Termius.deb
-sudo dpkg -i Termius.deb
-sudo apt install -f
-
 # Install Signal
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo apt update && sudo apt install signal-desktop -y
-
-# Install Spotify
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update
-sudo apt install spotify-client -y
-
-# Install Ookla Speedtest
-wget https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-x86_64-linux.tgz -O ookla-speedtest.tgz
-tar -xzf ookla-speedtest.tgz
-sudo mv speedtest /usr/local/bin/
 
 
