5998851eb86d0023f0427f433bd37e0c9321698c
JMcglock <85720969+jmcglock@users.noreply.github.com>
2022-12-20 13:53:25 -0500

    onedriver

--- linux/fresh-ubuntu-desktop-install.sh
+++ linux/fresh-ubuntu-desktop-install.sh
@@ -33,4 +33,9 @@ curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 sudo apt update && sudo apt install signal-desktop -y
 
+# Install OneDriver
+echo 'deb http://download.opensuse.org/repositories/home:/jstaf/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:jstaf.list
+curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_jstaf.gpg > /dev/null
+sudo apt update
+sudo apt install onedriver
 
