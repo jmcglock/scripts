8599ccf59059b7e820e3f1f8a6a2d271efb7d6eb
JMcglock <85720969+jmcglock@users.noreply.github.com>
2022-12-23 11:17:01 -0800

    include that -y

--- linux/setup-pihole-instance.sh
+++ linux/setup-pihole-instance.sh
@@ -2,7 +2,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Install the required dependencies
-sudo apt-get install git dnsutils bc
+sudo apt-get install git dnsutils bc -y
 
 # Clone the Pi-hole installation script from GitHub
 git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
