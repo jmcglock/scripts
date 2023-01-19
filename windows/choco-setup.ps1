d76915a9804c01ff9805de8011f3ae485b486e38
JMcglock <85720969+jmcglock@users.noreply.github.com>
2023-01-19 17:06:01 -0500

    updated

--- windows/choco-setup.ps1
+++ windows/choco-setup.ps1
@@ -1,31 +1,14 @@
-#install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-ECHO Installing apps
-
-ECHO Configure chocolatey
-
-choco feature enable -n allowGlobalConfirmation
-
-choco install termius
-choco install 7zip
-choco install bleachbit
-choco install choco-cleaner 
-choco install chocolateygui
-choco install kubens
-choco install kubernetes-cli
-choco install kubernetes-helm
-choco install kubernetes-kompose
-choco install speedtest
-choco install brave
-choco install starship
-choco install vscode
-choco install mysql.workbench
-choco install jetbrainstoolbox
-choco install git
-choco install python
-choco install signal
-choco install discord
-choco install spotify
-
-choco feature disable -n allowGlobalConfirmation
+#install Chocolatey 
+$apps = @("termius","7zip","bleachbit","choco-cleaner","chocolateygui","kubens","kubernetes-cli","kubernetes-helm","kubernetes-kompose","speedtest","brave","starship","vscode","mysql.workbench","jetbrainstoolbox","git","python","signal","discord","spotify")
+ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+ECHO Installing apps 
+ECHO Configure chocolatey 
+choco feature enable -n allowGlobalConfirmation 
+ Function Install-Chocolatey {
+    Param ($app)
+    choco install $app
+}
+ foreach ($app in $apps) {
+    Install-Chocolatey $app
+}
+ choco feature disable -n allowGlobalConfirmation
