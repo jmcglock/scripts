ba7898b3e828349f6880dc70aea3a9c7d09b3c53
JMcglock <85720969+jmcglock@users.noreply.github.com>
2023-01-19 17:06:47 -0500

    updated

--- windows/hourly-windows-update.ps1
+++ windows/hourly-windows-update.ps1
@@ -1,15 +1,12 @@
-# Set up variables for the task
-$taskName = "Hourly Update Check"
-$taskDescription = "Checks for updates every hour as an administrator"
-$taskCommand = "wuauclt.exe"
-$taskArguments = "/detectnow"
-$taskStartTime = (Get-Date).Date.AddHours(1)
-
-# Create the scheduled task
-$action = New-ScheduledTaskAction -Execute "$taskCommand" -Argument "$taskArguments"
-$trigger = New-ScheduledTaskTrigger -Daily -At 1:00
-$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
-$settings = New-ScheduledTaskSettingsSet
-
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName `
+# Set up variables for the task 
+$taskName = "Hourly Update Check" 
+$taskDescription = "Checks for updates every hour as an administrator" 
+$taskCommand = "wuauclt.exe /detectnow" 
+$taskStartTime = (Get-Date).Date.AddHours(1) 
+# Create the scheduled task 
+$action = New-ScheduledTaskAction -Execute "$taskCommand" 
+$trigger = New-ScheduledTaskTrigger -Daily -At 1:00 
+$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount 
+$settings = New-ScheduledTaskSettingsSet 
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName ` 
                       -Description $taskDescription -Principal $principal -Settings $settings
