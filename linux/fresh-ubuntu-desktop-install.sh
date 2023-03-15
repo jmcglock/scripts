f34a6e830895de2502e60be3937e7814a104d61f
JMcglock <85720969+jmcglock@users.noreply.github.com>
2023-03-14 22:55:50 -0400

    optimize

--- linux/fresh-ubuntu-desktop-install.sh
+++ linux/fresh-ubuntu-desktop-install.sh
@@ -1,45 +1,18 @@
 # Update the package list and install all updates
-sudo apt update
-sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 # Install snap
+sudo apt update && sudo apt install snapd && sudo snap install snap-store
+
+# Install Applications
 sudo apt update
-sudo apt install snapd
-sudo snap install snap-store
-
-# Install Visual Studio Code
 sudo snap install code --classic
-
-# Install Discord
 sudo snap install discord
-
-# Install Signal
 sudo snap install signal-desktop
-
-# Install Termius
 sudo snap install termius-app
-
-# Install OneDriver
 echo 'deb http://download.opensuse.org/repositories/home:/jstaf/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:jstaf.list
 curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_jstaf.gpg > /dev/null
-sudo apt update
-sudo apt install onedriver
+sudo apt install onedriver gnome-tweaks space spotify starship brave
 
-# Install gnome-tweaks
-sudo apt install gnome-tweaks
-
-# Install Space
-sudo snap install space
-
-# Install Spotify
-sudo snap install spotify
-
-# Install Starship
-sudo snap install starship
-
-# Install Brave
-sudo snap install brave
-
-# Install 
-
+# Reboot
 sudo reboot
